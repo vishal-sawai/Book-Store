23fc338d84a843557c4739129f57651656c17f80
vishal sawai <vishalsawai1725@gmail.com>
2024-01-27 23:24:13 +0530

    Add MongoDB connection and book routes

--- server/config.js
+++ server/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://root:coder@book-store-mern.ibstunt.mongodb.net/book-collection?retryWrites=true&w=majority'
--- server/index.js
+++ server/index.js
@@ -1,9 +1,86 @@
 import express from "express";
-import { PORT } from './config.js'
+import { PORT, mongoDBURL } from './config.js'
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`);
+// Middleware for passing request body
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    console.log("Hello Guys in console");
+    return res.status(234).send('welcome to Mern Stack way');
+})
+
+app.post('/books', async (request, response) => {
+    try {
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Send All Required field: title,author,publishYear',
+            });
+        }
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
+        const book = await Book.create(newBook);
+        return response.status(201).send(book);
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
 });
 
+// Route for get all books from database
+app.get('/books', async (request, response) => {
+    try {
+        const books = await Book.find({});
+
+        return response.status(200).json({
+            count: books.length,
+            data: books
+        })
+    }
+    catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+})
+
+
+// Gets book by id
+app.get('/books/:id', async (request, response) => {
+    try {
+
+        const { id } = request.params;
+        const book = await Book.findById(id);
+
+        return response.status(200).json({
+            count: books.length,
+            data: books
+        });
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message })
+    }
+})
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App coonected to database');
+        app.listen(PORT, () => {
+            console.log(`App is listening to port:${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    })
