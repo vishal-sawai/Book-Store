0e165e5a57e5bc80e722d71122b60809773690de
vishal sawai <vishalsawai1725@gmail.com>
2024-01-28 23:10:25 +0530

    Refactor DeleteBook component imports

--- client/src/pages/DeleteBook.jsx
+++ client/src/pages/DeleteBook.jsx
@@ -2,11 +2,11 @@ import React, { useState } from 'react';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
 import axios from 'axios';
-import { useNavigate, useNavigation, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const DeleteBook = () => {
     const [loading, setLoading] = useState(false);
-    const navigate = useNavigation();
+    const navigate = useNavigate();
     const { id } = useParams();
 
     const handleDeleteBook = () => {
@@ -37,4 +37,4 @@ const DeleteBook = () => {
         </div>
     )
 }
-export default DeleteBook
+export default DeleteBook;
